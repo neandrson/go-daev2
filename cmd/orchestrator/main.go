762d3a3aa499d63bf02a7ea1ff6279069d71c316
neandrson <a2361@yandex.ru>
2025-03-03 19:46:40 +0300

    Update main.go

--- cmd/orchestrator/main.go
+++ cmd/orchestrator/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/neandrson/go-daev2/internal/orchestrator/application"
 	"github.com/neandrson/go-daev2/internal/orchestrator/config"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
 	cfg, err := config.NewConfigFromEnv()
