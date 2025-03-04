5baeaabc9e8e3597506d16f201badec0a71cdad7
neandrson <62153258+neandrson@users.noreply.github.com>
2025-03-04 08:05:34 +0300

    Update main.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,19 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/neandrson/go-daev2/internal/agent/application"
 	"github.com/neandrson/go-daev2/internal/agent/config"
 )
-
-func init() {
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
-}
 
 func main() {
 	cfg, err := config.NewConfigFromEnv()
