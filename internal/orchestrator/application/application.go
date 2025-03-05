53532ba83dd90dba86a286c623e73fc958f66ad8
neandrson <a2361@yandex.ru>
2025-03-05 19:39:30 +0300

    Update application.go

--- internal/orchestrator/application/application.go
+++ internal/orchestrator/application/application.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/joho/godotenv"
 	"github.com/neandrson/go-daev2/internal/http/server"
 	"github.com/neandrson/go-daev2/internal/orchestrator/config"
 )
@@ -22,10 +21,6 @@ func NewApplication(cfg *config.Config) *Application {
 }
 
 func (orch *Application) Run(ctx context.Context) int {
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
-
 	logger := log.New(os.Stderr, "Orchestrator: ", log.Ldate|log.Ltime|log.Lmsgprefix)
 
 	shutDownFunc, err := server.Run(ctx, logger, orch.cfg)
